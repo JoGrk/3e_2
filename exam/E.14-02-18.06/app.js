d28ee152c488ac2b27b40cde54dcafa83f06f23b
Jola Gryko <jgryko@gmail.com>
2023-01-19 15:46:26 +0100

    bar

---
+++ exam/E.14-02-18.06/app.js
@@ -0,0 +1,9 @@
+const rekrut  = document.getElementById("rekrut");
+const form = document.querySelector("form");
+
+form.style.getPropertyValue = NONE;
+
+rekrut.addEventListener("click",()=>{
+    const position = document.getElementById('position').value;
+    console.log(position);
+});
